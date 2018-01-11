a26082a12501452ab83645ee7d5616ddb6a4f5e9
Karl Fogel <kfogel@red-bean.com>
2018-01-11 01:24:45 -0600

    Improve introductory comment in mailaprop.el

--- mailaprop.el
+++ mailaprop.el
@@ -1,4 +1,4 @@
-;;;; Mailaprop: a custom completion system for my email contacts.
+;;;; Mailaprop: popup-style autofill for email addresses in Emacs.
 ;;;
 ;;; Copyright (C) 2007-2018  Karl Fogel
 ;;; 
@@ -14,6 +14,8 @@
 ;;; 
 ;;; You should have received a copy of the GNU Affero General Public License
 ;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Home page: https://github.com/kfogel/mailaprop/
 
 ;; TODO: 
 ;;
