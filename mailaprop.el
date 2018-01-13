73a21aa7b1b8a779f824690e9be3e2e147cb99bc
Karl Fogel <kfogel@red-bean.com>
2018-01-12 23:16:56 -0600

    Improve minibuffer messages
    Include the string "mailaprop" in the minibuffer messages shown while
    `mailaprop-load-addresses' is loading the addresses and building the
    cache.  That way if it takes a long time, the culprit will be clear.

--- mailaprop.el
+++ mailaprop.el
@@ -155,14 +155,16 @@ RAW-ADDRESSES is a list as read from `mailaprop-address-file'."
   (interactive)
   (if (file-exists-p mailaprop-address-file)
       (let ((memoized-strings ()))
-        (message "Reading addresses and rebuilding autofill cache...")
+        (message 
+         "Reading mailaprop addresses and rebuilding autofill cache...")
         (maphash (lambda (key ignored-val)
                    (setq memoized-strings (cons key memoized-strings)))
                  mailaprop-memoize-dict)
         (mailaprop-digest-raw-addresses (mailaprop-inhale-addresses))
         (clrhash mailaprop-memoize-dict)
         (dolist (str memoized-strings) (mailaprop-get-candidates str))
-        (message "Reading addresses and rebuilding autofill cache...done"))
+        (message 
+         "Reading mailaprop addresses and rebuilding autofill cache...done"))
     (error "You must set `mailaprop-address-file'.")))
 (defalias 'mailaprop-reload-addresses 'mailaprop-load-addresses)
 
