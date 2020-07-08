0279c0722d28a26c4d5f602a7b2804933c53b835
Karl Fogel <kfogel@red-bean.com>
2020-07-07 22:49:17 -0500

    Make the test suite a real test suite

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-# This is not a real test suite yet, but it's a first step.
-# You have to just read the output and see if it looks right.
-
 cat test-data/*.mbox                                         \
   | ./mailaprop.py --skip-regexps test-data/skip-regexps.txt \
-  > test-output.eld
+  > test-data/test-output.eld
 
-cat test-output.eld
+if cmp test-data/test-output.eld test-data/expected-output.txt; then
+  echo "All tests passed."
+  rm test-data/test-output.eld
+else
+  echo "TESTS FAILED.  SEE DETAILS BELOW." >&2
+  echo "" >&2
+  diff -u test-data/expected-output.txt test-data/test-output.eld
+  echo "" >&2
+  echo "TESTS FAILED.  SEE DETAILS ABOVE." >&2
+fi
